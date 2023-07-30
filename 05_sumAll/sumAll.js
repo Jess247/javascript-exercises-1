daec1a8e51511d346c45470527516847a0240acc
Jess247 <jesss8692@hotmail.com>
2023-07-30 15:44:30 +0200

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+/*
+get two ints as arguments
+create sum variable 
+sum all numbers starting form the num1 up to num2 and save them in sum variable
+return the sum after the loop
+*/
+const sumAll = function(firstNum, secondNum) {
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return "ERROR";
+    }
+    if (firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    }
+    let smallerNum = (firstNum < secondNum) ? firstNum : secondNum;
+    let higherNum = (firstNum > secondNum) ? firstNum : secondNum;;
+    
+    let sum = 0;
 
+    
+    while (smallerNum <= higherNum) {
+        sum += smallerNum;
+        smallerNum++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
