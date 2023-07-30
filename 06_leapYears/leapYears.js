d4bbeee9ee7d5eba201ff85c1d4f6feb4779f6fe
Jess247 <jesss8692@hotmail.com>
2023-07-30 16:05:36 +0200

    Complete exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+/*
+is year divisible by four
+is year not divisible by 100 but by 400
+*/
+return (year % 4 === 0 && year % 100 != 0 || year % 400 === 0);
 
 };
 
